e5733922eccb240c755618014a4b9f3cafffb455
tasuku43 <task.teckac@gmail.com>
2026-08-24 22:07:53 +0900

    Sync public snapshot and retire WP11 packet

--- docs/work/workspace-template-context-policy-memory/check_batch_a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-packages='./internal/domain/tobari ./internal/infra/workspaceauthoritystore ./internal/app/workspaceauthoritycmd ./internal/cli'
-tests='Test(ContextAuthorityAxes|PublishWorkspaceEntryAuthority|FinalDefaultPair|InitializeFinalDefaultPair|FreshDefaultPairEntry|ContextEntry|DefaultPair|FinalContextProjection|FinalContextHuman|BareStatus)'
-
-go test $packages -run "$tests" -count=1
-go test -race $packages -run "$tests" -count=1
